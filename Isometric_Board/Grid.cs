47caf236233113407426054e71be04738bc7a136
danielronalds <danielronalds@mmc.school.nz>
2020-08-25 14:30:58 +1200

    Rough implementation of an isometric board that is 6*6. Super annoying to work with, so I am going to rework it. PROOF OF CONCEPT

--- Isometric_Board/Grid.cs
+++ Isometric_Board/Grid.cs
@@ -9,8 +9,8 @@ namespace Isometric_Board
 {
     class Grid
     {
-        int x_offset = 300;
-        int y_offset = 65;
+        int x_offset = 350;
+        int y_offset = 140;
 
         public Point A1, A2, A3, A4, A5, A6;
 
@@ -26,42 +26,42 @@ namespace Isometric_Board
 
         public Grid()
         {
-            A1 = new Point(x_offset + 81, y_offset + 84);
+            A1 = new Point(x_offset + (22*1), y_offset + (11*1));
             A2 = new Point(A1.X - 22, A1.Y + 11);
             A3 = new Point(A2.X - 22, A2.Y + 11);
             A4 = new Point(A3.X - 22, A3.Y + 11);
             A5 = new Point(A4.X - 22, A4.Y + 11);
             A6 = new Point(A5.X - 22, A5.Y + 11);
 
-            B1 = new Point(x_offset + 103, y_offset + 95);
+            B1 = new Point(x_offset + (22 * 2), y_offset + (11 * 2));
             B2 = new Point(B1.X - 22, B1.Y + 11);
             B3 = new Point(B2.X - 22, B2.Y + 11);
             B4 = new Point(B3.X - 22, B3.Y + 11);
             B5 = new Point(B4.X - 22, B4.Y + 11);
             B6 = new Point(B5.X - 22, B5.Y + 11);
 
-            C1 = new Point(x_offset + 125, y_offset + 106);
+            C1 = new Point(x_offset + (22 * 3), y_offset + (11 * 3));
             C2 = new Point(C1.X - 22, C1.Y + 11);
             C3 = new Point(C2.X - 22, C2.Y + 11);
             C4 = new Point(C3.X - 22, C3.Y + 11);
             C5 = new Point(C4.X - 22, C4.Y + 11);
             C6 = new Point(C5.X - 22, C5.Y + 11);
 
-            D1 = new Point(x_offset + 147, y_offset + 117);
+            D1 = new Point(x_offset + (22 * 4), y_offset + (11 * 4));
             D2 = new Point(D1.X - 22, D1.Y + 11);
             D3 = new Point(D2.X - 22, D2.Y + 11);
             D4 = new Point(D3.X - 22, D3.Y + 11);
             D5 = new Point(D4.X - 22, D4.Y + 11);
             D6 = new Point(D5.X - 22, D5.Y + 11);
 
-            E1 = new Point(x_offset + 169, y_offset + 128);
+            E1 = new Point(x_offset + (22 * 5), y_offset + (11 * 5));
             E2 = new Point(E1.X - 22, E1.Y + 11);
             E3 = new Point(E2.X - 22, E2.Y + 11);
             E4 = new Point(E3.X - 22, E3.Y + 11);
             E5 = new Point(E4.X - 22, E4.Y + 11);
             E6 = new Point(E5.X - 22, E5.Y + 11);
 
-            F1 = new Point(x_offset + 191, y_offset + 139);
+            F1 = new Point(x_offset + (22 * 6), y_offset + (11 * 6));
             F2 = new Point(F1.X - 22, F1.Y + 11);
             F3 = new Point(F2.X - 22, F2.Y + 11);
             F4 = new Point(F3.X - 22, F3.Y + 11);
